4b1ae2723b28c8a78a6b859557b65323bdefb521
Donovan Ainsley <donovan_weise@live.co.uk>
2024-03-21 16:20:39 +0000

    Allow user to submit to database and display as a card on index.html

--- app.py
+++ app.py
@@ -90,8 +90,21 @@ def profile(username):
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_method": request.form.get("recipe_method"),
+            "image_url": request.form.get("image_url")
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Successfully Added")
+        return redirect(url_for("home"))
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template ("add_recipe.html", categories=categories)
 
