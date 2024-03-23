3a7196c436dba3663131b22dc7e8b9e40779750f
Donovan Ainsley <donovan_weise@live.co.uk>
2024-03-23 05:48:21 +0000

    added collapsible jquery from materialize

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('select').formSelect();
   });
+
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
+    
