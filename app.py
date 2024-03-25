f8384fa6a2307482b239b9da1b3b988d8a7f89d7
Donovan Ainsley <donovan_weise@live.co.uk>
2024-03-25 19:37:09 +0000

    amended profile function to display user created recipes

--- app.py
+++ app.py
@@ -80,12 +80,12 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    # grab the session user's username from db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
 
-    if session["user"]:
-        return render_template("profile.html", username=username)
+    user_recipes = list(mongo.db.recipes.find({"created_by": session["user"]}))
+
+    if session.get("user"):
+        return render_template("profile.html", 
+        username=username, user_recipes=user_recipes)
 
     return redirect(url_for("login"))
 
