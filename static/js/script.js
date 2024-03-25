995632da3e81c3a969eeb5ed656718d422a98f55
Donovan Ainsley <donovan_weise@live.co.uk>
2024-03-25 10:51:38 +0000

    added defensive modal to prevent accidental deletion of recipes

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('select').formSelect();
+    $('.modal').modal();
   });
 
 var elem = document.querySelector('.collapsible.expandable');
